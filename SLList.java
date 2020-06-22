27a715b7501e1df7039a8d69e5e030e721c4ebcf
Beeeam <59039964+Beeeam@users.noreply.github.com>
2020-06-22 15:16:52 +0800

    how to calculate size efficiently
    using the variable size

--- SLList.java
+++ SLList.java
@@ -9,16 +9,20 @@ public class SLList{
         }
     }
     private IntNode first;
+    private int size;
 
     public SLList(int x){
         first = new IntNode(x,null);
+        size = 1;
     }
 
     public void addFirst(int x){
         first = new IntNode(x, first);
+        size += 1;
     }
 
     public void addLast(int x){
+        size += 1;
         IntNode p = first;
         while(p.next!= null){
             p = p.next;
@@ -26,15 +30,18 @@ public class SLList{
         p.next = new IntNode(x, null);
     }
     
-    private static int size(IntNode p){
-        if(p.next==null){
-            return 1;
-        }
-        return 1+size(p.next);
-    }
+//     private static int size(IntNode p){
+//         if(p.next==null){
+//             return 1;
+//         }
+//         return 1+size(p.next);
+//     }
 
+//     public int size(){
+//         return size(first);    
+//     }
     public int size(){
-        return size(first);    
+        return size;
     }
     
     public int getFirst(){
